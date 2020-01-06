3f18a1c4cef336292a9566ef0dc8f7d853c88b8c
luannd <kid1110nd@gmail.com>
2020-01-06 14:01:08 +0700

    up code

--- PauseTokenSource/PauseToken.cs
+++ PauseTokenSource/PauseToken.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace PauseTokenSource
 {
     class PauseToken
     {
         private readonly PauseTokenSource m_source;
+
         internal PauseToken(PauseTokenSource source) { m_source = source; }
 
         public bool IsPaused { get { return m_source != null && m_source.IsPaused; } }
--- PauseTokenSource/PauseTokenSource.cs
+++ PauseTokenSource/PauseTokenSource.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 
 namespace PauseTokenSource
@@ -37,6 +33,7 @@ namespace PauseTokenSource
                 }
             }
         }
+
         public PauseToken Token { get { return new PauseToken(this); } }
 
         internal Task WaitWhilePausedAsync()
